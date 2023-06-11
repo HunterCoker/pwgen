f15a3bf657519fb21ca080cdd6a3a7a8219372aa
Hunter Coker <hunter.coker.2016@gmail.com>
2023-06-10 22:23:43 -0400

    documentation

--- pwgen.cpp
+++ pwgen.cpp
@@ -2,10 +2,17 @@
 
 #include <random>
 
+/**
+ * uses the command line arguments to get the length and optional conditions that the password must meet when generated
+ * 
+ * @param n number of command line arguments given by the user
+ * @param tokens null terminated strings of each argument given by the user
+ * @return an exit code specifying whether the given arguments were valid
+ */
 bool pwgen::process_command(int n, char* tokens[]) {
     // no arguments given
     if (n == 1) {
-        std::cout << "error: missing argument(s)... usage: 'pwgen [OPTIONS] [LENGTH--REQUIRED]' or see '--help'\n";
+        std::cout << "error: missing argument(s)... usage: 'pwgen [-OPTIONS] [LENGTH--REQUIRED]' or see '--help'\n";
         return false;
     }
 
@@ -62,7 +69,11 @@ bool pwgen::process_command(int n, char* tokens[]) {
     return true;
 }
 
-// using palette conditions, generates valid symbols for desired password
+/**
+ * uses input conditions to generate valid symbols for a desired password of a specified length
+ * 
+ * @return a randomly generated password under certain conditions
+ */
 std::string pwgen::gen_password() const {
     int palette_size;
     char* palette = create_palette(&palette_size);
@@ -76,7 +87,7 @@ std::string pwgen::gen_password() const {
 
 char* pwgen::create_palette(int* size) const {
     *size = 0;
-    char* palette = static_cast<char*>(malloc(88));
+    char* palette = static_cast<char*>(malloc(85));
     if (m_conditions & (1u << 0)) { memcpy(palette + *size, "abcdefghijklmnopqrstuvwxyz", 27); *size += 26; }
     if (m_conditions & (1u << 1)) { memcpy(palette + *size, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 27); *size += 26; }
     if (m_conditions & (1u << 2)) { memcpy(palette + *size, "0123456789"                , 11); *size += 10; }
@@ -87,7 +98,7 @@ char* pwgen::create_palette(int* size) const {
 void pwgen::print_help_message() {
     std::cout <<
         "PWGEN USAGE:\n"
-        "\tpwgen [OPTIONS] [LENGTH--REQUIRED]\n"
+        "\tpwgen [-OPTIONS] [LENGTH--REQUIRED]\n"
         "OPTIONS:\n"
         "\t-l\tlowercase - includes lowercase letters in the generated password.\n"
         "\t-u\tuppercase - includes uppercase letters in the generated password.\n"
